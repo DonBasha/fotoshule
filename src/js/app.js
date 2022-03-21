e7678d9ccd8b184d1480997853a11f309de4540c
DonBasha <100792191+DonBasha@users.noreply.github.com>
2022-03-21 19:13:33 +0100

    asd
    asd

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,44 @@ require('../scss/style.scss');
 
 /********** Paste your code here! ************/
 
-$(window).on("load",()=>{
-    $("body").addClass("hasLoaded")
+$(window).on("load", () => {
+  $("body").addClass("hasloaded")
+  setTimeout(() => {
+    $('.banner-content').addClass('active')
+  }, 1500)
+$('.header-wrapper').clipthru()
+
 })
+$("#menu-button").on("click touch", () => {
+  if ($("header").hasClass("menu-open")) {
+    $("header").removeClass("menu-open")
+  }
+  else {
+    $("header").addClass("menu-open")
+  }
+
+})
+$('.slick-slider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1
+});
+$('.accordeon-header').on('click touch',(e)=>{
+  const parent=$(e.currentTarget).parent()
+  const content=parent.find('.accordeon-content')
+  //$('.accordeon-item').removeClass('active')
+  //$('.accordeon-content').slideUp()
+  if(parent.hasClass('active')){
+    parent.removeClass('active')
+    content.slideUp()
+  }
+  else{
+    parent.addClass('active')
+    content.slideDown()
+  }
+})
+$('.gallery-list .col-lg-4').each((nr,el)=>{
+  const index = nr%3 * 0.3
+  $(el).addClass('wow fadeInUp').attr('data-wow-delay',index+'s')
+})
+new WOW().init();
